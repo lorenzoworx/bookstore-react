36812326370c9b199ae6f3f6bb0f65524d5863cc
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-06-13 16:27:35 +0100

    ADD EVENT HANDLER TO REMOVE BOOKS

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,22 +1,27 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-const Book = ({ book }) => (
-  <li>
-    <div className="bookContainer">
-      <h2>{book.Title}</h2>
-      <p>{book.Author}</p>
-      <div className="actionsContainer">
-        <button type="button">like</button>
-        <button type="button">Comment</button>
-        <button type="button">delete</button>
+const Book = ({ book }) => {
+  const dispatch = useDispatch();
+  return (
+    <li>
+      <div className="bookContainer">
+        <h2>{book.Title}</h2>
+        <p>{book.Author}</p>
+        <div className="actionsContainer">
+          <button type="button">edit</button>
+          <button type="button">Comment</button>
+          <button type="button" onClick={() => dispatch(removeBook(book.item_id))}>delete</button>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 Book.propTypes = {
   book: PropTypes.shape({
-    ID: PropTypes.number,
+    item_id: PropTypes.string,
     Title: PropTypes.string,
     Author: PropTypes.string,
   }).isRequired,
